5eeae01de55abce1521c4e2c0511a274ac1e6e25
Juan Cota <145627909+Juanlo01@users.noreply.github.com>
2026-08-02 19:46:44 -0700

    Added Evidence Viewer
    Added Evidence Viewer including a secondary camera with the Evidence Room and a piece of evidence with a modified version of the interaction script so that the evidence can be clicked and held

--- Assets/Player/3D/InteractableEvidence.cs
+++ Assets/Player/3D/InteractableEvidence.cs
@@ -1,12 +1,85 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using Yarn.Unity;
+using System.Collections;
 
 public class InteractableEvidence : MonoBehaviour, IInteractable3D{
     [SerializeField] private DialogueRunner dialogueRunner;
     [SerializeField] private string dialogueNode = "Start";
+    [SerializeField] private InputAction press, screenPos;
 
     private PlayerController3D interactingPlayer;
     private System.Action onInteractionEnd;
+
+    private Vector3 currentScreenPos;
+
+    Camera camera;
+    bool isDragging;
+
+    Vector3 lockPos;
+    float lockRot = -90.0f;
+
+    private void Update()
+    {
+        lockPos = new Vector3(transform.position.x, 4.0f, transform.position.z);
+        transform.rotation = Quaternion.Euler(lockRot, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+
+        if (transform.position.y >= 4.0f)
+        {
+            transform.position = lockPos;
+            transform.Translate(Vector3.down * Time.deltaTime);
+        }   
+    }
+    private Vector3 WorldPos
+    {
+        get
+        {
+            float z = camera.WorldToScreenPoint(transform.position).z;
+            return camera.ScreenToWorldPoint(currentScreenPos + new Vector3(0, 0, z));   
+        }
+    }
+    private bool isClickedOn
+    {
+        get
+        {
+            Ray ray = camera.ScreenPointToRay(currentScreenPos);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit))
+            {
+                return hit.transform == transform;
+            }
+            return false;
+        }
+    }
+
+    public void Awake()
+    {
+        camera = Camera.main;
+        screenPos.Enable();
+        press.Enable();
+        screenPos.performed += context => { currentScreenPos = context.ReadValue<Vector2>();};
+        press.performed += _ => { if(isClickedOn) StartCoroutine(Drag()); };
+        press.canceled += _ => {isDragging = false; };
+    }
+
+    private IEnumerator Drag()
+    {
+        isDragging = true;
+        Vector3 offset = transform.position - WorldPos;
+        // grab
+        GetComponent<Rigidbody>().useGravity = false; 
+        while (isDragging)
+        {
+            // dragging
+            transform.position = WorldPos + offset;
+            yield return null;
+            
+        }
+        // drop
+        GetComponent<Rigidbody>().useGravity = true;
+
+    }
 
     // interact(), for any class interfacing IInteractable3D
     public void Interact(PlayerController3D interactingPlayer, System.Action onInteractionEnd){
