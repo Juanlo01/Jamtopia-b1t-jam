f3ff931d139d8df400705105b227894be799513e
Diego Pereyra <133953132+deeg-p23@users.noreply.github.com>
2026-08-03 21:15:52 -0700

    Revert "Merge branch 'main' of https://github.com/Juanlo01/Jamtopia-b1t-jam into deeg/environment-init"
    This reverts commit ee9dc9cb54a0f46d67b7c42d1da0fdc4406d24d6, reversing
    changes made to b53bf08748c719b3a1913db4a8a38807ede06e4b.

--- Assets/Player/3D/InteractableCharacter.cs
+++ Assets/Player/3D/InteractableCharacter.cs
@@ -36,14 +36,4 @@ public class InteractableCharacter : MonoBehaviour, IInteractable3D{
         interactingPlayer = null;
         onInteractionEnd = null;
     }
-
-    public void OnTouchingPlayer()
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public void OnNotTouchingPlayer()
-    {
-        throw new System.NotImplementedException();
-    }
 }
