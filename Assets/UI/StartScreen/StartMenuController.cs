11fc32fd05261ee274913e04de88350e0c7118cc
Diego Pereyra <133953132+deeg-p23@users.noreply.github.com>
2026-08-10 10:30:36 -0700

    Fix NullReferenceException in StartMenuController.ChangeScene
    ChangeScene() is wired directly as the Play button's UGUI OnClick()
    handler, so it doesn't need to look up a UIDocument/UI Toolkit button
    and subscribe a listener to it (that GameObject has no UIDocument,
    hence the NRE) -- it just needs to perform the transition. Also fixes
    the scene name casing ("motel" -> "Motel") to match the actual scene
    file, otherwise the lookup would silently fail.

--- Assets/UI/StartScreen/StartMenuController.cs
+++ Assets/UI/StartScreen/StartMenuController.cs
@@ -1,28 +1,13 @@
 using UnityEngine;
-using UnityEngine.UIElements;
 using UnityEngine.SceneManagement;
 
 public class StartMenuController : MonoBehaviour
 {
-    private VisualElement uiStartMenu;
-
-        // private void OnEnable()
-        // {
-        //     // One-line query & click listener setup
-        //     GetComponent<UIDocument>().rootVisualElement.Q<Button>("PlayButton").clicked += () => 
-        //     {
-        //         Debug.Log("Button clicked!");
-        //         SceneManager.LoadScene("Motel");
-        //     };
-        // }
-
-        void ChangeScene()
-        {
-            // One-line query & click listener setup
-            GetComponent<UIDocument>().rootVisualElement.Q<Button>("PlayButton").clicked += () =>
-            {
-                Debug.Log("Button clicked!");
-                SceneManager.TransitionTo("Motel");
-            };
-        }
+    // wired directly as the Play button's OnClick() target in the Inspector, so this IS the click
+    // handler already -- it doesn't need to go find and subscribe to another button's click event
+    void ChangeScene()
+    {
+        Debug.Log("Button clicked!");
+        SceneManager.TransitionTo("Motel");
+    }
 }
