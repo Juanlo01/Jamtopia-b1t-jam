9404b67e2f5205995eafe7d46f462f690f7a1608
Juan Cota <145627909+Juanlo01@users.noreply.github.com>
2026-08-09 18:56:21 -0700

    Start Menu + Evidence List & Notebook

---
+++ Assets/UI/StartScreen/StartMenuController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+
+public class StartMenuController : MonoBehaviour
+{
+    private VisualElement uiStartMenu;
+
+        private void OnEnable()
+        {
+            // One-line query & click listener setup
+            GetComponent<UIDocument>().rootVisualElement.Q<Button>("PlayButton").clicked += () => 
+            {
+                Debug.Log("Button clicked!");
+                SceneManager.LoadScene("Motel");
+            };
+        }
+}
